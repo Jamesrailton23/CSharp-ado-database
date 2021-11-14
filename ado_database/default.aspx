6aae844954deb3fca61e898fedab1e7aaede3149
JamesRailton03 <jamesrailton23@gmail.com>
2021-11-14 01:14:00 +0000

    Add project files.

---
+++ ado_database/default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ado_database._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            name -
+            <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            address -
+            <asp:TextBox ID="tbAddress" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            postcode -
+            <asp:TextBox ID="tbPostCode" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            notes -<asp:TextBox ID="tbNotes" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            city -
+            <asp:TextBox ID="tbCity" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="buttonEnterData" runat="server" OnClick="buttonEnterData_Click" Text="Enter" />
+            <br />
+            <br />
+            <br />
+        </div>
+        <asp:GridView ID="gridviewCustomers" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ ado_database/default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ado_database
+{
+	public partial class _default : System.Web.UI.Page
+	{
+		
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			displaycustomers();
+			
+		}
+		public static void AddCustomer(Customer newCustomer)
+		{
+			// this takes in the results from button click method and adds it to the database 
+			var customer = new Customer();
+			customer.CustomerID = newCustomer.CustomerID;
+			customer.name = newCustomer.name;
+			customer.address = newCustomer.address;
+			customer.City = newCustomer.City;
+			customer.ZipCode = newCustomer.ZipCode;
+			customer.Notes = newCustomer.Notes;
+
+			ACME db = new ACME();
+			var dbcustomers = db.Customers;
+		
+
+			dbcustomers.Add(customer);
+			db.SaveChanges();	
+		}
+
+		protected void buttonEnterData_Click(object sender, EventArgs e)
+		{
+			// this take in the infomation from the textboxs and pass's into the addCustomer method, also calls the display customer method which updates the gridview
+
+			var newCustomer = new Customer();
+			newCustomer.CustomerID = Guid.NewGuid();
+			newCustomer.name = tbName.Text;
+			newCustomer.address = tbAddress.Text;
+			newCustomer.City = tbCity.Text;
+			newCustomer.ZipCode = tbPostCode.Text;
+			newCustomer.Notes = tbNotes.Text;
+
+			AddCustomer(newCustomer);
+			displaycustomers();
+
+
+		}
+
+		private void displaycustomers()
+		{
+			//  Updates the gridview to show new database entrites also
+
+			ACME db = new ACME();
+			
+			// the data sorce must be as list to work, have to call ADO model and not database
+
+			gridviewCustomers.DataSource = db.Customers.ToList();
+			gridviewCustomers.DataBind();
+
+		}
+	}
+}
